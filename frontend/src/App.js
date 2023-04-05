bf67e0bf8cf2035969f7a38f64c5e14cd14906cd
Soumyadip1245 <soumyadip20csu214@ncuindia.edu>
2023-04-05 23:12:31 +0530

    Login and Register with blur effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from './component/header/Header'
 import Section from './component/section/Section'
 import Login from './component/login/Login'
-
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
   return (
     <>
-      <Login />
-      {/* <Header />
-      
-      <Section /> */}
+      <Header />
+      <div className="App">
+        {!isLoggedIn && (
+          <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)", zIndex: 1 }}>
+            <Login onLogin={handleLogin} />
+          </div>
+        )}
+        <div style={{ filter: isLoggedIn ? "none" : "blur(5px)" }}>
+          <Section />
+        </div>
+      </div >
     </>
   )
 }
--- frontend/src/component/login/Login.jsx
+++ frontend/src/component/login/Login.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 const Login = () => {
-    return (
-        <div className="logo-page">
+    const [showRegisterForm, setShowRegisterForm] = useState(false);
+
+    const register = () => {
+        setShowRegisterForm(true);
+    };
+    const login = () => {
+        setShowRegisterForm(false)
+    }
+    if (showRegisterForm) {
+        return (
+            <div className="form">
+                <div className="logo-container">
+                    <img src="https://www.cipherschools.com/static/media/Cipherschools_icon@2x.3b571d743ffedc84d039.png" alt="Your Logo" />
+                    <h2>CipherSchools</h2>
+                </div>
+                <div className="input-row">
+                    <input
+                        type="email"
+                        className="input-box"
+                        placeholder="Email ID"
+                    />
+                </div>
+                <div className="input-row">
+                    <input
+                        type="password"
+                        className="input-box"
+                        placeholder="Enter Password"
+                    />
+                </div>
+                <button class="login-btnn" ><span>Register</span></button>
+                <div className="input-bottom">
+                    <div className="input-account">Done with Sign in ?</div>
+                    <a onClick={login}>  <span>Login</span></a>
+                </div>
+
+            </div>
+
+        )
+    }
+    else {
+        return (
             <div className="form">
                 <div className="logo-container">
                     <img src="https://www.cipherschools.com/static/media/Cipherschools_icon@2x.3b571d743ffedc84d039.png" alt="Your Logo" />
@@ -25,13 +64,14 @@ const Login = () => {
                 <button class="login-btnn" ><span>Signin</span></button>
                 <div className="input-bottom">
                     <div className="input-account">Don't have an account ?</div>
-                    <span>Get Started</span>
+                    <a onClick={register}>  <span>Get Started</span></a>
                 </div>
 
             </div>
-        </div>
 
-    )
+        )
+    }
+
 }
 
 export default Login
