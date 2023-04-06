28b8e346ff8e67144dbdc9d4b65f0d57ce8fa497
Soumyadip1245 <soumyadip20csu214@ncuindia.edu>
2023-04-06 12:04:57 +0530

    Login API

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -6,7 +6,7 @@ var ObjectId = require('mongoose').Types.ObjectId;
 router.get('/ok', (req, res) => {
     res.json({ message: "All Working" })
 })
-
+//Registration
 router.post('/register', (req, res) => {
     bcrypt.hash(req.body.password, 10, (err, hash) => {
         const data = new User({
@@ -21,6 +21,30 @@ router.post('/register', (req, res) => {
         }).catch(() => {
             res.json({ message: "All Fields Are Required", success: false })
         })
+    })
+})
+//Login
+router.post('/login', (req, res) => {
+    User.find({ email: req.body.email }).then((value) => {
+        if (value.length < 1) {
+            res.json({ success: false, message: "Email Not Found For Details" })
+        }
+        else {
+            const user = value[0]
+            bcrypt.compare(req.body.password, user.password, (err, value) => {
+                if (value) {
+                    const payload = {
+                        userId: user._id,
+                        userName: user.name
+                    }
+                    const token = jwt.sign(payload, "webBatch")
+                    res.json({ success: true, message: "Login Successful", token: token })
+                }
+                else {
+                    res.json({ success: false, message: "Credentials Not Matched For Email" })
+                }
+            })
+        }
     })
 })
 module.exports = router
